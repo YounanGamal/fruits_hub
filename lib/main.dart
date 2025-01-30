319aa591d3afcebc436edacbfa88fc565aafae7c
Younan <younangamal722@gmail.com>
2025-01-30 16:45:42 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitsHUB());
+}
+
+class FruitsHUB extends StatelessWidget {
+  const FruitsHUB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
